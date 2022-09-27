1dfa329338b909733252057a4ff8adaa8d1cfa32
Cin2323 <95028396+Cin2323@users.noreply.github.com>
2022-09-27 18:18:50 +0300

    Initial commit

---
+++ src/main/java/com/example/myapplication/model/Gender.java
@@ -0,0 +1,5 @@
+package com.example.myapplication.model;
+
+public enum Gender {
+    MASCULIN,FEMININ
+}
---
+++ src/test/java/com/example/myapplication/serviceTest/ProductTest.java
@@ -0,0 +1,20 @@
+package com.example.myapplication.serviceTest;
+
+import com.example.myapplication.model.Product;
+import com.example.myapplication.service.ProductService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+public class ProductTest {
+
+//    @Autowired
+//    ProductService productService;
+//
+//    @Test
+//    public void addProductTest(){
+//        Product product = new Product();
+//        Assertions.assertNotNull( productService.addProduct(product));
+//    }
+}
